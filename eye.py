8e695a2b2dcc754a350ac63cd758f1eb4ed90736
1000day <vettel122411@gmail.com>
2026-05-16 20:00:51 +0800

    顯示眼睛

---
+++ eye.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+import numpy as np
+from copy import deepcopy
+
+img = np.array(Image.open("105724566.png")).astype(np.float16)
+
+print(img.shape)
+
+BIAS_Y = 17
+BIAS_X = 23
+SIZE = 45
+
+for y in range(img.shape[0]):
+    for x in range(img.shape[1]):
+        r, g, b, a = img[y][x]
+        if r+b-g > (r+g+b)/3*1.5:
+            img[y][x] = [255, 255, 255, 255]
+        else:
+            img[y][x] = [0, 0, 0, 0]
+
+eye_left = [deepcopy(img[y:y+SIZE, x:x+SIZE]) for y in (BIAS_Y+SIZE*2,) for x in range(BIAS_X, img.shape[1], SIZE) if x+SIZE <= img.shape[1]]
+eye_right = [deepcopy(im[:, ::-1]) for im in eye_left]
+
+for i, eye in enumerate( zip(eye_left, eye_right) ):
+    img_eye = np.concatenate(eye, axis=1)
+    Image.fromarray(img_eye.astype(np.uint8)).save(f"110_eye_{i}.png")
+
+for y in range(BIAS_Y, img.shape[0], SIZE):
+    for x in range(img.shape[1]):
+        img[y][x] = [255, 0, 0, 255]
+
+for x in range(BIAS_X, img.shape[1], SIZE):
+    for y in range(img.shape[0]):
+        img[y][x] = [255, 0, 0, 255]
+    
+Image.fromarray(img.astype(np.uint8)).save("110_mask.png")
--- tft.py
+++ tft.py
@@ -1,3 +1,7 @@
+from eye import eye_left, eye_right
+import numpy as np
+import random
+
 import st7735
 from PIL import Image, ImageDraw, ImageFont
 from gpiozero import DigitalOutputDevice
@@ -21,29 +25,63 @@ disp = st7735.ST7735(
     dc=24, 
     rst=25, 
     width=132, 
-    height=160, 
-    rotation=90, 
-    spi_speed_hz=4000000
+    height=162, 
+    # rotation=90, 
+    rotation=0,
+    spi_speed_hz=4000000,
+    invert=False
 )
 
-font_size = 8
+
+def genEye(eyes):
+    e = random.choice(eyes)
+    while True:
+        if random.random() < 0.3:
+            e = random.choice(eyes)
+            for _ in range(9):
+                yield e
+
+        yield e
+
+gen_left = genEye(eye_left)
+gen_right = genEye(eye_right)
+
 
 while True:
-    # 建立畫布並繪製內容
-    img = Image.new('RGB', (160, 128), color=(0, 0, 0))
-    draw = ImageDraw.Draw(img)
+    for i, eye in enumerate( zip(eye_left, eye_right) ):
+        img = np.zeros((128, 180, 4), dtype=np.float16)
+        
+        y0 = img.shape[0]//2 - next(gen_left).shape[0]//2
+        x0 = img.shape[1]//2 - next(gen_left).shape[1]//2 - 20
+        e = next(gen_left)
+        img[y0:y0+e.shape[0], x0:x0+e.shape[1]] = e.astype(np.float16)
 
-    my_font = ImageFont.truetype(
-        "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 
-        font_size
-    ) 
+        x0 = img.shape[1]//2 + 20
+        e = next(gen_right)
+        img[y0:y0+e.shape[0], x0:x0+e.shape[1]] = e.astype(np.float16)
 
-    draw.text((10, 10), f"{font_size}pt: Hello Raspberry Pi!", fill=(255, 255, 255), font=my_font)
+        disp.display(Image.fromarray(np.transpose(img.clip(0, 255).astype(np.uint8), (1, 0, 2))[::-1,:, :3]))
+        sleep(1/60)
 
-    # 顯示影像
-    disp.display(img)
-    sleep(0.5)
 
-    font_size += 1
-    if font_size > 16:
-        font_size = 8
+# while True:
+#     font_size = 8
+    
+#     # 建立畫布並繪製內容
+#     img = Image.new('RGB', (160, 128), color=(0, 0, 0))
+#     draw = ImageDraw.Draw(img)
+
+#     my_font = ImageFont.truetype(
+#         "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 
+#         font_size
+#     ) 
+
+#     draw.text((10, 10), f"{font_size}pt: Hello Raspberry Pi!", fill=(255, 255, 255), font=my_font)
+
+#     # 顯示影像
+#     disp.display(img)
+#     sleep(0.5)
+
+#     font_size += 1
+#     if font_size > 16:
+#         font_size = 8

590552b47fe31bede8f38ae03ddc70f1f131f959
1000day <vettel122411@gmail.com>
2026-05-16 20:11:05 +0800

    36 eyes

--- eye.py
+++ eye.py
@@ -3,6 +3,8 @@ from PIL import Image
 import numpy as np
 from copy import deepcopy
 
+from os import makedirs
+
 img = np.array(Image.open("105724566.png")).astype(np.float16)
 
 print(img.shape)
@@ -19,12 +21,17 @@ for y in range(img.shape[0]):
         else:
             img[y][x] = [0, 0, 0, 0]
 
-eye_left = [deepcopy(img[y:y+SIZE, x:x+SIZE]) for y in (BIAS_Y+SIZE*2,) for x in range(BIAS_X, img.shape[1], SIZE) if x+SIZE <= img.shape[1]]
+eye_left = [deepcopy(img[y:y+SIZE, x:x+SIZE]) for y in range(BIAS_Y, img.shape[0]-SIZE, SIZE) for x in range(BIAS_X, img.shape[1]-SIZE, SIZE)]
 eye_right = [deepcopy(im[:, ::-1]) for im in eye_left]
+
+
+
+DIR = "eye_check"
+makedirs(DIR, exist_ok=True)
 
 for i, eye in enumerate( zip(eye_left, eye_right) ):
     img_eye = np.concatenate(eye, axis=1)
-    Image.fromarray(img_eye.astype(np.uint8)).save(f"110_eye_{i}.png")
+    Image.fromarray(img_eye.astype(np.uint8)).save(f"{DIR}/110_eye_{i}.png")
 
 for y in range(BIAS_Y, img.shape[0], SIZE):
     for x in range(img.shape[1]):
@@ -34,4 +41,4 @@ for x in range(BIAS_X, img.shape[1], SIZE):
     for y in range(img.shape[0]):
         img[y][x] = [255, 0, 0, 255]
     
-Image.fromarray(img.astype(np.uint8)).save("110_mask.png")
+Image.fromarray(img.astype(np.uint8)).save(f"{DIR}/110_mask.png")

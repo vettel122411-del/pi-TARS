2b0bb987282b2d7a3ce4e239eeea787e458edad2
1000day <vettel122411@gmail.com>
2026-05-21 19:22:17 +0800

    Add walk.py for servo control and movement functionality

--- walk.py
+++ walk.py
@@ -14,28 +14,30 @@ for i in range(2):
 	pca.servo[i].set_pulse_width_range(config["range"][i]["lo"], (config["range"][i]["hi"]))
 
 
-pca.servo[0].angle = 180-config["move"][0]["A0"]
-pca.servo[1].angle = config["move"][1]["A0"]
-sleep(0.5)
+def zeroing():
+	pca.servo[0].angle = 180-config["move"][0]["A0"]
+	pca.servo[1].angle = config["move"][1]["A0"]
 
-if len(sys.argv) >= 2 and sys.argv[1]=="zeroing":
-	exit(0)
-
-sleep(1.0)
-
-while True:
-	pca.servo[0].angle = 180 -config["move"][0]["A0"] - config["move"][0]["A1"]
+def move_forward():
+	pca.servo[0].angle = 180-config["move"][0]["A0"] - config["move"][0]["A1"]
 	pca.servo[1].angle = config["move"][1]["A0"] + config["move"][1]["A1"]
-	sleep(0.6)
-
-	if len(sys.argv) >= 2 and sys.argv[1]=="forward":
-		exit(0)
+	sleep(0.4)
 
 	for da in range(config["misc"]["splits"], 0, -1):
 		ratio = da/config["misc"]["splits"]
 		pca.servo[0].angle = 180-config["move"][0]["A0"] - ratio*config["move"][0]["A1"]
 		pca.servo[1].angle = config["move"][1]["A0"] + ratio*config["move"][1]["A1"]
 		sleep(5e-3)
-	
-	sleep(0.4)
+
+	sleep(0.3)
+
+
+
+zeroing()
+sleep(0.5)
+
+while True:
+	move_forward()
+
+
 	
